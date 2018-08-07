3c7b55c644639f1ea33d82d01ce343e9a146352c
Amanda Bullington <amanda@albull.us>
2018-08-06 18:59:52 -0700

    Cleaned up styling for Header

--- app/components/Header/HeaderLink.js
+++ app/components/Header/HeaderLink.js
@@ -5,8 +5,8 @@ export default styled(Link)`
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   font-weight: bold;
   font-size: 16px;
-  border: 2px solid #41addd;
   color: #41addd;
+  margin-right: 20px;
   &:active {
     background: #41addd;
     color: #fff;
--- app/components/Header/NavBar.js
+++ app/components/Header/NavBar.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  text-align: center;
+  text-align: left;
+  background-color: #555;
 `;
